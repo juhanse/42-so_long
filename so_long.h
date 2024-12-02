90e747404725640474c34de03d0cd2d1101a68fd
Julien Hanse <julien.hanse12@gmail.com>
2024-12-02 12:22:34 +0100

    add: struct in header file

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:46:35 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/02 12:15:17 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/02 12:21:43 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,11 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <math.h>
+
+typedef struct s_vars
+{
+	int	x;
+	int	y;
+}	t_vars;
 
 #endif
--- src/ft_utils.c
+++ src/ft_utils.c
@@ -6,8 +6,21 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:45:33 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/02 12:11:48 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/02 12:19:32 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+int	key_handler(int keycode, t_vars *vars)
+{
+    if (keycode == KEY_ARROW_RIGHT)
+        vars->player.pos.x += 1;
+    else if (keycode == KEY_ARROW_LEFT)
+        vars->player.pos.x -= 1;
+    else if (keycode == KEY_ARROW_UP)
+        vars->player.pos.y -= 1;
+    else if (keycode == KEY_ARROW_DOWN)
+        vars->player.pos.y += 1;
+    return (0);
+}
