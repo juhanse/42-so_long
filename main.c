fb2f5689a9f94908219d853c369ee29e84359bd2
Julien Hanse <julien.hanse12@gmail.com>
2024-12-02 12:06:21 +0100

    add: test in main function

--- main.c
+++ main.c
@@ -6,11 +6,19 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:16:38 by juhanse           #+#    #+#             */
-/*   Updated: 2024/11/06 14:20:45 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/02 12:05:47 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "so_long.h"
+#include <mlx.h>
+
 int	main(void)
 {
-	return (0);
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
