9bc20d9d987204fc62c08c4a506b1cccc58e4f88
Julien Hanse <julien.hanse12@gmail.com>
2024-12-02 12:16:10 +0100

    add: import math & fcntl lib

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:46:35 by juhanse           #+#    #+#             */
-/*   Updated: 2024/11/26 12:47:07 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/02 12:15:17 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <math.h>
 
 #endif
