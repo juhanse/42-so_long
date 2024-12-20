20cde391269a52a4f9b15ed10dd7003b78466421
juhanse <julien.hanse12@gmail.com>
2024-12-20 01:42:43 +0100

    add: mlx in header

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 12:46:35 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/02 12:21:43 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/12/20 01:42:16 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <math.h>
+# include <mlx.h>
 
 typedef struct s_vars
 {
