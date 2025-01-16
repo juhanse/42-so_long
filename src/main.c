fea856b2b57f76d67aa0bda37c94ccebc9f0e6c4
juhanse <julien.hanse12@gmail.com>
2025-01-16 14:15:02 +0100

    renamed: title window

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:16:38 by juhanse           #+#    #+#             */
-/*   Updated: 2024/12/20 15:02:51 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/01/16 14:11:26 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(void)
 
 	y = -1;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, X, Y, "Cool");
+	mlx_win = mlx_new_window(mlx, X, Y, "So Long");
 	while (++y < Y)
 	{
 		x = -1;
@@ -30,4 +30,5 @@ int	main(void)
 			mlx_pixel_put(mlx, mlx_win, x, y, 0xff0000);
 	}
 	mlx_loop(mlx);
+	return (0);
 }
