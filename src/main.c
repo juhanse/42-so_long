3cfba9167395db3869a3446785d0a80feda56d7f
juhanse <julien.hanse12@gmail.com>
2025-01-17 21:15:11 +0100

    add: initialize function

--- src/main.c
+++ src/main.c
@@ -6,29 +6,43 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:16:38 by juhanse           #+#    #+#             */
-/*   Updated: 2025/01/16 14:28:20 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/01/17 21:14:54 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 #include "../mlx/mlx.h"
 
-int	main(void)
+void	ft_initialize(t_map *map, char **av)
 {
-	int		x;
-	int		y;
-	void	*mlx;
-	void	*mlx_win;
+	map->filename = av[1];
+	map->player.y = 0;
+	map->player.x = 0;
+	map->y = 0;
+	map->x = 0;
+	map->c = 0;
+	map->p = 0;
+	map->e = 0;
+	map->exit = 0;
+	map->move = 0;
+	map->check_c = 0;
+	map->check_e = 0;
+}
 
-	y = -1;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, X, Y, "So Long");
-	while (++y < Y)
+int	main(int argc, char **argv)
+{
+	t_map	map;
+
+	if (argc != 2)
 	{
-		x = -1;
-		while (++x < X)
-			mlx_pixel_put(mlx, mlx_win, x, y, 0xff0000);
+		printf("Invalid arguments\n");
+		return (0);
 	}
-	mlx_loop(mlx);
+	map_init(&map, argv);
+	map.mlx = mlx_init();
+	map.wnd = mlx_new_window(map.mlx, map.x * IMG_PXL, map.y * IMG_PXL, WND_NAME);
+/* 	mlx_hook(map.wnd, 17, 0, ft_close, &map);
+	mlx_key_hook(map.wnd, key_move, &map); */
+	mlx_loop(map.mlx);
 	return (0);
 }
