e471553d35bb893d1345a60ab7457864036e53bf
juhanse <julien.hanse12@gmail.com>
2025-01-21 15:14:51 +0100

    refactor: load image prototype

--- src/game.c
+++ src/game.c
@@ -6,18 +6,20 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 14:21:11 by juhanse           #+#    #+#             */
-/*   Updated: 2025/01/21 15:09:04 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/01/21 15:14:15 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 #include "../mlx/mlx.h"
 
-void	load_img(t_map *map, int index, char *path)
+void	load_img(t_map *map, char *path, int index)
 {
 	if (!path)
 		exit(EXIT_FAILURE);
 	map->assets[index].path = path;
+	map->assets[index].width = IMG_PXL;
+	map->assets[index].height = IMG_PXL;
 	map->assets[index].img = mlx_xpm_file_to_image(map->mlx, path, &map->assets[index].width, &map->assets[index].height);
 	if (!map->assets[index].img)
 	{
@@ -45,7 +47,7 @@ void	start_game(t_map *map)
 {
 	map->mlx = mlx_init();
 	map->wnd = mlx_new_window(map->mlx, X, Y, WND_NAME);
-	load_img(map, 1, "./assets/wall.xpm");
+	load_img(map, "./assets/wall.xpm", 1);
 	fill_game(map);
 	mlx_loop(map->mlx);
 }
