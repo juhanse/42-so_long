97ba3e4abf8e7961370ca6b96f3dc0842a81b32c
juhanse <julien.hanse12@gmail.com>
2025-02-03 11:09:39 +0100

    add: backtracking debug

--- src/backtracking.c
+++ src/backtracking.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 16:12:33 by juhanse           #+#    #+#             */
-/*   Updated: 2025/02/03 10:49:26 by juhanse          ###   ########.fr       */
+/*   Updated: 2025/02/03 11:07:13 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,14 @@ int	flood_fill(t_map *map, int x, int y, int *collects)
 		return (0);
 	if (map->copy[x][y] == '1')
 		return (0);
+	printf("Exploring (%d, %d)\n", x, y);
 	if (map->copy[x][y] == 'E')
 		return (*collects == 0);
 	if (map->copy[x][y] == 'C')
+	{
 		(*collects)--;
+		map->copy[x][y] = '1';
+	}
 	map->copy[x][y] = '1';
 	if (flood_fill(map, x - 1, y, collects) || flood_fill(map, x + 1, y, collects) || flood_fill(map, x, y - 1, collects) || flood_fill(map, x, y + 1, collects))
 		return (1);
@@ -75,12 +79,6 @@ void	ft_map_reachable(t_map *map)
 	collects = map->collects;
 	ft_copy_map(map);
 	result = flood_fill(map, map->player.y, map->player.x, &collects);
-	int i = -1;
-	while (++i < map->line)
-	{
-		printf("[%d] %s\n", i, map->copy[i]);
-		printf("[%d] %s\n", i, map->map[i]);
-	}
 	if (!result || collects != 0)
 	{
 		printf("Map is not reachable\n");
