f5e68c8c02a5326e1a230a4266f8e580e99febbc
juhanse <julien.hanse12@gmail.com>
2025-02-12 12:58:40 +0100

    add: debug mode gnl

--- src/gnl/get_next_line.c
+++ src/gnl/get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/19 11:09:06 by juhanse           #+#    #+#             */
-/*   Updated: 2025/02/11 17:15:50 by juhanse          ###   ########.fr       */
+/*   Created: 2025/02/01 11:09:06 by juhanse           #+#    #+#             */
+/*   Updated: 2025/02/12 12:58:17 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,117 +120,10 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	stash = add_static(stash);
+	// if (!stash)
+	// {
+	// 	free(stash);
+	// 	stash = NULL;
+	// }
 	return (line);
 }
-
-// void	next_call(t_stack *stack)
-// {
-// 	t_gnl	*new_node;
-// 	char	*buffer;
-
-// 	if (!stack || !stack->top)
-// 		return ;
-// 	buffer = extract_after_newline(stack);
-// 	if (!buffer)
-// 		return ;
-// 	new_node = malloc(sizeof(t_gnl));
-// 	if (!new_node)
-// 	{
-// 		free(buffer);
-// 		return ;
-// 	}
-// 	new_node->str = buffer;
-// 	new_node->next = NULL;
-// 	deallocate(stack, new_node, buffer);
-// }
-
-// char	*get_da_line(t_stack *stack)
-// {
-// 	int		len;
-// 	char	*str_ln;
-
-// 	len = len_to_ln(stack);
-// 	str_ln = malloc(len + 1);
-// 	if (!str_ln)
-// 		return (NULL);
-// 	ft_strcpy(str_ln, stack);
-// 	return (str_ln);
-// }
-
-// void	add_stack(t_stack *stack, char *buffer)
-// {
-// 	t_gnl	*new_node;
-// 	t_gnl	*last;
-
-// 	new_node = malloc(sizeof(t_gnl));
-// 	if (new_node == NULL)
-// 		return ;
-// 	if (stack->size < 1)
-// 		stack->top = new_node;
-// 	else
-// 	{
-// 		last = stack->top;
-// 		while (last->next != NULL)
-// 			last = last->next;
-// 		last->next = new_node;
-// 	}
-// 	new_node->next = NULL;
-// 	new_node->str = buffer;
-// 	stack->size++;
-// }
-
-// void	create(t_stack *list, int fd)
-// {
-// 	char	*buffer;
-// 	int		char_read;
-
-// 	while (!new_line(list))
-// 	{
-// 		buffer = malloc(BUFFER_SIZE + 1);
-// 		if (!buffer)
-// 			return ;
-// 		char_read = read(fd, buffer, BUFFER_SIZE);
-// 		if (char_read < 0)
-// 		{
-// 			free(buffer);
-// 			deallocate(list, NULL, NULL);
-// 			return ;
-// 		}
-// 		if (char_read == 0)
-// 		{
-// 			free(buffer);
-// 			return ;
-// 		}
-// 		buffer[char_read] = '\0';
-// 		add_stack(list, buffer);
-// 	}
-// }
-
-// char	*get_next_line(int fd)
-// {
-// 	static t_stack	*node = NULL;
-// 	char			*get_next_ln;
-
-// 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &get_next_ln, 0) < 0)
-// 		return (NULL);
-// 	if (!node)
-// 	{
-// 		node = malloc(sizeof(t_stack));
-// 		if (!node)
-// 			return (NULL);
-// 		node->top = NULL;
-// 		node->size = 0;
-// 	}
-// 	create(node, fd);
-// 	if (!new_line(node) && node->size == 0)
-// 	{
-// 		free(node);
-// 		node = NULL;
-// 		return (NULL);
-// 	}
-// 	get_next_ln = get_da_line(node);
-// 	if (!get_next_ln)
-// 		return (deallocate(node, NULL, NULL), NULL);
-// 	next_call(node);
-// 	return (get_next_ln);
-// }
--- src/gnl/get_next_line_utils.c
+++ src/gnl/get_next_line_utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/19 11:14:11 by juhanse           #+#    #+#             */
-/*   Updated: 2025/02/11 17:14:20 by juhanse          ###   ########.fr       */
+/*   Created: 2025/02/01 11:14:11 by juhanse           #+#    #+#             */
+/*   Updated: 2025/02/12 12:58:10 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,10 @@ char	*ft_strdup(const char *src)
 
 char	*ft_strjoin_gnl(char const *s1, char const *s2)
 {
-	char	*str;
+	int		i;
 	int		len_s1;
 	int		len_s2;
-	int		i;
+	char	*str;
 
 	if (!s1 && !s2)
 		return (NULL);
@@ -82,128 +82,3 @@ char	*ft_strjoin_gnl(char const *s1, char const *s2)
 	str[i] = '\0';
 	return (str);
 }
-
-// int	len_to_ln(t_stack *stack)
-// {
-// 	int		i;
-// 	int		len;
-// 	t_gnl	*current;
-
-// 	if (stack == NULL)
-// 		return (0);
-// 	len = 0;
-// 	current = stack->top;
-// 	while (current != NULL)
-// 	{
-// 		i = 0;
-// 		while (current->str[i])
-// 		{
-// 			if (current->str[i] == '\n')
-// 			{
-// 				len++;
-// 				return (len);
-// 			}
-// 			i++;
-// 			len++;
-// 		}
-// 		current = current->next;
-// 	}
-// 	return (len);
-// }
-
-// char	*ft_strcpy(char *str, t_stack *stack)
-// {
-// 	int		i;
-// 	int		j;
-// 	t_gnl	*current;
-
-// 	j = 0;
-// 	current = stack->top;
-// 	while (current != NULL)
-// 	{
-// 		i = 0;
-// 		while (current->str[i])
-// 		{
-// 			if (current->str[i] == '\n')
-// 			{
-// 				str[j++] = '\n';
-// 				str[j] = '\0';
-// 				break ;
-// 			}
-// 			str[j++] = current->str[i++];
-// 		}
-// 		current = current->next;
-// 	}
-// 	str[j] = '\0';
-// 	return (str);
-// }
-
-// void	deallocate(t_stack *list, t_gnl *new_node, char *buffer)
-// {
-// 	t_gnl	*temp;
-
-// 	while (list->top != NULL)
-// 	{
-// 		temp = list->top->next;
-// 		free(list->top->str);
-// 		free(list->top);
-// 		list->top = temp;
-// 	}
-// 	list->top = NULL;
-// 	list->size = 0;
-// 	if (new_node && new_node->str && new_node->str[0])
-// 	{
-// 		list->top = new_node;
-// 		list->size = 1;
-// 	}
-// 	else
-// 	{
-// 		free(new_node);
-// 		free(buffer);
-// 	}
-// }
-
-// int	new_line(t_stack *stack)
-// {
-// 	int		i;
-// 	t_gnl	*current;
-
-// 	current = stack->top;
-// 	while (current != NULL)
-// 	{
-// 		i = 0;
-// 		while (current->str[i] && i < BUFFER_SIZE)
-// 		{
-// 			if (current->str[i] == '\n')
-// 				return (1);
-// 			i++;
-// 		}
-// 		current = current->next;
-// 	}
-// 	return (0);
-// }
-
-// char	*extract_after_newline(t_stack *stack)
-// {
-// 	char	*buffer;
-// 	t_gnl	*last;
-// 	int		i;
-// 	int		j;
-
-// 	i = 0;
-// 	j = 0;
-// 	last = stack->top;
-// 	while (last && last->next != NULL)
-// 		last = last->next;
-// 	buffer = malloc(BUFFER_SIZE + 1);
-// 	if (!last || !buffer)
-// 		return (NULL);
-// 	while (last->str[i] != '\n' && last->str[i] != '\0')
-// 		i++;
-// 	if (last->str[i] == '\n')
-// 		i++;
-// 	while (last->str[i] != '\0')
-// 		buffer[j++] = last->str[i++];
-// 	buffer[j] = '\0';
-// 	return (buffer);
-// }
